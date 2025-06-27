b6bd3120233db958e1377af0404eb7cde24e0a00
pranavi3 <pranavi.nukala0302@gmail.com>
2025-06-27 15:09:28 +0530

    Added TODOs

--- src/main/java/com/np/urlShortener/controller/URLGenerationController.java
+++ src/main/java/com/np/urlShortener/controller/URLGenerationController.java
@@ -26,3 +26,9 @@ public class URLGenerationController {
         return new ResponseEntity<>(generateResponse, HttpStatus.CREATED);
     }
 }
+
+//TODO: Implement Caching on Generation - check the cache and then DB for existing URL and update the cache too on generation
+//TODO: Update click count on Redirection asynchronously - store it cache and periodically update in DB (may be cron jobs or async workers)
+//TODO: Implement Analytics Endpoint - check the cache and then DB
+//TODO: Test cases
+//TODO: Deployment
